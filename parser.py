b7ca803b30de85c9fdc94f4e0186be8cf3ec5436
William Lorey <wwlorey@gmail.com>
2017-06-29 07:48:26 -0500

    Fixed comments because I'm not a barbarian

--- parser.py
+++ parser.py
@@ -1,36 +1,40 @@
 import sys
 
+# Read command line arguments
 progName, fileInName, fileOutName = sys.argv
+
+# Open the input & output files
 fileIn = open(fileInName, 'r')
 fileOut = open(fileOutName, 'w')
 
-css = fileIn.read().replace('\n', '') #not super necessary thing to do
-                                      #(remove '\n' instances) but is a good
-                                      #safety measure
+# Remove all newlines & tabs from the input as a safety measure
+css = fileIn.read().replace('\n', '').replace('\t', '')
+
 length = len(css)
 for pos in range(0, length):
-    ### character processing ###
-    #commas and colons
+    ### Character processing ###
+    # Commas and colons
     if css[pos] == ',' or css[pos] == ':':
         prevChar = ''
         nextChar = ' '
-    #opening curly brackets
+    # Opening curly brackets
     elif css[pos] == '{':
         prevChar = ' '
         nextChar = '\n'
-    #semicolons
+    # Semicolons
     elif css[pos] == ';':
         prevChar = ''
         nextChar = '\n'
-    #closing curly brackets
+    # Closing curly brackets
     elif css[pos] == '}':
         prevChar = '\n'
         nextChar = '\n\n'
-    #everyting else
+    # Everyting else
     else:
         nextChar = ''
         prevChar = ''
 
+    # Write the current character(s) to the output file
     fileOut.write("%s%s%s" % (prevChar, css[pos], nextChar))
 
 fileIn.close()
